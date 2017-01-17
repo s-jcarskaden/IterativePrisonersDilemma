a2c48d5db0ea01cf67895d37ee7614cc60f52859
s-jcarskaden <s-jcarskaden@lwsd.org>
2017-01-17 08:44:16 -0800

    Updateteam3.py
    Updated name, strategy name, and strategy description

--- team3.py
+++ team3.py
@@ -1,14 +1,14 @@
 ####
 # Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
+#     team_name: Team J&J
+#     strategy_name: Best Strategy
+#     strategy_description: this strategy always works
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Team J&J' # Only 10 chars displayed.
+strategy_name = 'Best Strategy'
+strategy_description = 'The Strategy that will always work'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
